3b65350a0a1e2f2da6dcfe477c558c39c38185fd
Robert Beckwith <rjbeckwith55@gmail.com>
2017-08-21 22:08:08 -0500

    edited

---
+++ Lab3.29/Lab3.29/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number\tSquare\tCube");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(i + "\t" + i * i + "\t" + i * i * i);
+            }
+            Console.ReadKey();
+        }
+    }
+}

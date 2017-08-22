1d7037b5db0033c96aab44831262f290b11f5bef
Robert Beckwith <rjbeckwith55@gmail.com>
2017-08-21 23:33:21 -0500

    completed lab 2.10

---
+++ Lab3.29/Lab3.24/OddEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._24
+{
+    class OddEven
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter in a number");
+            num = Convert.ToInt32(Console.Read());
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Odd");
+
+            }
+            else
+                Console.WriteLine("Even");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab3.29/Lab3.28/FiveDigits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._28
+{
+    class FiveDigits
+    {
+        static void Main(string[] args)
+        {
+            String Number = "";
+            while (Number.Length != 5)
+            {
+                Console.Write("Enter in a five digit number: ");
+                Number = Console.ReadLine();
+            }
+            for (int i = 0; i < Number.Length; i++)
+            {
+                Console.Write(Number[i] + "   ");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}

e7d26bdf2ed1719025214fb953a75d913bbde430
Robert Beckwith <rjbeckwith55@gmail.com>
2017-08-21 22:39:09 -0500

    Lab number 2

---
+++ Lab3.29/Lab3.17/Comparison.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._17
+{
+    class Comparison
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            int average, sum, product, largest,smallest;
+
+            //Prompt and read input
+            Console.WriteLine("Input the first integer: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the second integer: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the third integer: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            //Calculate the sum average product max and min
+            sum = num1 + num2 + num3;
+            average = sum/3;
+            product = num1 * num2 * num3;
+            largest = num1;
+            smallest = num1;
+            if (num2 > largest)
+            {
+                largest = num1;
+            }
+            if (num3> largest)
+            {
+                largest = num3;
+            }
+            if (num2 < smallest)
+            {
+                smallest = num2;
+            }
+            if (num3 < smallest)
+            {
+                smallest = num3;
+            }
+            Console.WriteLine("Sum: "+ sum);
+            Console.WriteLine("Average: "+ average);
+            Console.WriteLine("Product: "+ product);
+            Console.WriteLine("Largest: "+ largest);
+            Console.WriteLine("Smallest: "+ smallest);
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- Lab3.29/Lab3.17/Properties/AssemblyInfo.cs
+++ Lab3.29/Lab3.17/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NNBB")]
+[assembly: AssemblyTitle("Lab3.17")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NNBB")]
+[assembly: AssemblyProduct("Lab3.17")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9aa657dc-c203-4a2f-ad2d-32dd6747428d")]
+[assembly: Guid("a38b434a-d050-4aa8-9a7f-db3371ef374d")]
 
 // Version information for an assembly consists of the following four values:
 //

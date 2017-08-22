d7702ecb205c9a254736ff7aec59644433d3d25c
Robert Beckwith <rjbeckwith55@gmail.com>
2017-08-21 22:43:28 -0500

    initial files for lab 3.17

--- Lab3.29/Lab3.17/Comparison.cs
+++ Lab3.29/Lab3.17/Comparison.cs
@@ -14,11 +14,11 @@ namespace Lab3._17
             int average, sum, product, largest,smallest;
 
             //Prompt and read input
-            Console.WriteLine("Input the first integer: ");
+            Console.Write("Input the first integer: ");
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Input the second integer: ");
+            Console.Write("Input the second integer: ");
             num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Input the third integer: ");
+            Console.Write("Input the third integer: ");
             num3 = Convert.ToInt32(Console.ReadLine());
 
             //Calculate the sum average product max and min
@@ -43,14 +43,27 @@ namespace Lab3._17
             {
                 smallest = num3;
             }
+
+            //Print out all the values
             Console.WriteLine("Sum: "+ sum);
             Console.WriteLine("Average: "+ average);
             Console.WriteLine("Product: "+ product);
             Console.WriteLine("Largest: "+ largest);
             Console.WriteLine("Smallest: "+ smallest);
+
             Console.ReadKey();
 
 
         }
     }
 }
+/*SAMPLE OUTPUT
+Input the first integer: 45
+Input the second integer: 66
+Input the third integer: 43
+Sum: 154
+Average: 51
+Product: 127710
+Largest: 45
+Smallest: 43
+*/

713dfa2da95c2a6009d61284af8404f0e591fdbc
Robert Beckwith <rjbeckwith55@gmail.com>
2017-09-28 08:39:54 -0500

    added palindrome file and compound interest file

---
+++ Compound interest/Compound interest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compound_interest
+{
+    class Program
+    {
+        // Compound interest rate calculator without using only integers
+        static void Main(string[] args)
+        {
+            int principal = 100000; //initial amount       before interest  
+            int amount = principal; //amount on deposit at end of year           
+
+            int rate = 5; //interest rate %
+            Console.WriteLine("Year\t Amount on deposit");
+            for (int year = 1; year <= 10; ++year)
+            {
+                amount = amount + ((amount * rate) / 100);
+                //display amount               
+                Console.WriteLine(""+year+"\t\t   $"+( amount/100)+"."+ amount % 100);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String numbers;
+            Console.WriteLine("Enter in a 5 digit nubmer to determine if it is a palindrome: ");
+            numbers = Console.ReadLine();
+            if (numbers.Length == 5)
+            {
+                if (numbers[0] == numbers[4])
+                {
+                    if (numbers[1] == numbers[3])
+                    {
+                        Console.WriteLine("This is a Palindrome");
+                    }
+                    else
+                        Console.WriteLine("Not a palindrome");
+                }
+                else
+                    Console.WriteLine("Not a palindrome");
+            }
+            else
+            {
+                while(numbers.Length != 5)
+                {
+                    Console.WriteLine("Enter in a 5 digit nubmer to determine if it is a palindrome: ");
+                    numbers = Console.ReadLine();
+                }
+            }
+        
+        }
+    }
+}

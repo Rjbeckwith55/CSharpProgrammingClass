7aab651cb0c3c0c82beab10a37f3a3f413daaf73
Robert Beckwith <rjbeckwith55@gmail.com>
2017-10-31 23:37:39 -0500

    uploaded files

---
+++ Perfect Numbers/Perfect Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // all the factors of this number will add up to equal the number
+            double perfect;
+            for (int i = 2; i < 1000; i++)
+            {
+                perfect =  Convert.ToDouble((Math.Pow(2, i - 1)) * (Math.Pow(2, i - 1)));
+                Console.WriteLine("Perfect numbers: " + perfect);
+            }
+            
+            
+        }
+    }
+}
---
+++ QualityPoints/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double score;
+            int scale;
+            Console.WriteLine("Input your average grade");
+            score = Convert.ToDouble(Console.ReadLine());
+            scale = QualityPoints(score);
+            Console.WriteLine("Your GPA is: "+scale);
+        }
+        static int QualityPoints(double score)
+        {
+            if (score >= 90)
+                return 4;
+            else if (score < 90 && score >= 80)
+                return 3;
+            else if (score < 80 && score >= 70)
+                return 2;
+            else if (score < 70 && score >= 60)
+                return 1;
+            else
+                return 0;
+
+        }
+
+    }
+}
